60218d90bb6e7fdc21f68cd065d467a383db9dca
Lokesh Dangare <lokeshdangare05@gmail.com>
2024-09-03 00:09:50 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Smart_TextCrunch_Engine.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from Smart_TextCrunch_Engine.pipeline.data_validation import DataValidationTrainingPipeline
 from Smart_TextCrunch_Engine.logging import logger
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -10,3 +11,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<< \n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+

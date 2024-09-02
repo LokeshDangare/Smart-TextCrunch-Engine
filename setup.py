9afa3eba3b3e3455bb946f3634d1e2283713b21f
Lokesh Dangare <lokeshdangare05@gmail.com>
2024-09-02 12:27:30 +0530

    requirements & installation done

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Smart-TextCrunch-Engine"
+AUTHOR_USER_NAME = "LokeshDangare"
+SRC_REPO = "Smart_TextCrunch_Engine"
+AUTHOR_EMAIL = "lokeshdangare05@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A text crunching engine for natural language processing tasks",
+    long_description=long_description,
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
